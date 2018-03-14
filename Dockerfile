4b4b950af2e3eea93b3189922e4a0ea4297ea0f8
Norawit Urailertprasert <norawit543@hotmail.com>
2018-03-14 22:49:12 +0700

    update

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,4 @@
 FROM php:7.0-apache
 COPY src/ /var/www/html/
+
+#  docker run -v /root/webapp-font/src/:/var/www/html/ -p 34555:80 php:7.0-apache
--- src/index.php
+++ src/index.php
@@ -1,7 +1,12 @@
+
+Welcome <?php echo ?><br>
+Your email address is: <?php echo $_POST["email"]; ?>
 <?php
-$cookie_name = "user";
-$cookie_value = "John Doe";
-setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/"); // 86400 = 1 day
+$old_font_name = $_COOKIE["font name"]
+$old_font_size = $_COOKIE["font size"]
+setcookie("font name",  $_POST["font name"], time() + (86400 * 30), "/"); // 86400 = 1 day
+setcookie("font size",  $_POST["font size"], time() + (86400 * 30), "/"); // 86400 = 1 day
+
 ?>
 <html>
 <body>
@@ -10,10 +15,21 @@ setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/"); // 86400 = 1
 if(!isset($_COOKIE[$cookie_name])) {
     echo "Cookie named '" . $cookie_name . "' is not set!";
 } else {
+    echo "<div><font face=\'".$old_font_name."\' size=".$old_font_size.">".$old_font_name."</font></div><br>"
+    echo "<div><font face=\'".$old_font_name."\' size=".$old_font_size.">".$old_font_size."</font></div><br>"
     echo "Cookie '" . $cookie_name . "' is set!<br>";
     echo "Value is: " . $_COOKIE[$cookie_name];
 }
 ?>
+<form action="http://128.199.88.139:34555">
+  your font type :<br>
+  <input type="text" name="font name" value="helvetica">
+  <br>
+  your font size:<br>
+  <input type="text" name="font size" value="12">
+  <br><br>
+  <input type="submit" value="Submit">
+</form> 
 
 </body>
 </html>
